f860db15ba5e26375d7d94cf74fd4f43ef12213c
julia <julia@LAPTOP-ILMLJSM->
2024-02-09 19:31:08 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//Napisz program, który za pomocą instrukcji do ... while wyświetla liczby całkowite z przedziału od 1 do 20 podniesione do kwadratu.
+//Wynik obliczeń wyświetl w konsoli.
+
+class PetlaDoWhile {
+    static void Main(string[] args)
+    {
+        int number = 1;
+        do
+        {
+            int square = number * number;
+            Console.WriteLine($"Liczba {number} podniesiona do kwadratu to: {square}.");
+            number++;
+        } while (number <= 20);
+    }
+}
